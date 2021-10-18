92481fa223441dbcbb569e62f4c281b1eaff7bb5
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-10-18 17:44:07 -0300

    Adiciona aula sobre interfaces

--- introducao-ao-typescript/app.ts
+++ introducao-ao-typescript/app.ts
@@ -1,1 +1,23 @@
-console.log('TypeScript');
+interface IAnimal {
+    name: string;
+    type: 'terrestre' | 'aquático';
+    executeGrow(volume: number): void;
+};
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean,
+};
+
+const animal: IAnimal = {
+    name: 'Elefante',
+    type: 'terrestre',
+    executeGrow: volume => `${volume}dB`,
+};
+
+animal.executeGrow(50);
+
+const felino: IFelino = {
+    name: 'Leão',
+    type: 'terrestre',
+    visaoNoturna: true,
+};
