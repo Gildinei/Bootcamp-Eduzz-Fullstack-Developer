1f5b24f71b4affdc73a21b4ea61aa35bdca22978
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-10-19 18:16:32 -0300

    Adiciona aula sobre validação de condicionais e parâmetros

--- introducao-ao-typescript/app.ts
+++ introducao-ao-typescript/app.ts
@@ -1,49 +1,12 @@
-const inputElement = document.getElementById('input') as HTMLInputElement;
-
-interface IAnimal {
-    name: string;
-    type: 'terrestre' | 'aquático';
-    executeGrow(volume: number): void;
+interface IUsuario {
+    id: string;
+    email: string;
 };
 
-interface IFelino extends IAnimal {
-    visaoNoturna: boolean,
+interface IAdmin extends IUsuario {
+    cargo: 'super' | 'master';
 };
 
-const animal: IAnimal = {
-    name: 'Elefante',
-    type: 'terrestre',
-    executeGrow: volume => `${volume}dB`,
-};
-
-type IPessoa = {
-    nome: string;
-    sobrenome: string;
-    idade: number;
-    sexo: 'Masculino' | 'Feminino';
-    altura: number;
-    peso: number;
-    online: boolean;
-};
-
-const pessoa: IPessoa = {
-    nome: 'Gildinei',
-    sobrenome: 'Anunciação',
-    idade: 30,
-    sexo: "Masculino",
-    altura: 1.83,
-    peso: 110,
-    online: true,
+function redirect(usuario: IUsuario | IAdmin) {
 
 };
-
-inputElement.addEventListener('input', (event) => {
-    const input = event.currentTarget as HTMLInputElement;
-    console.log(input.value);
-});
-
-function adicionaApendiceALista<T>(array: T[], valor: T) {
-    return array.map(() => valor);
-};
-
-adicionaApendiceALista([1, 2, 3], 4);
